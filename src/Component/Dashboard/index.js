09cc104bc5bdb7f1e635e8c87ebe20bb6cde4ef4
Code-4ge <panmandneeraj@gmail.com>
2022-03-04 20:25:32 +0530

    Added Dashboard

--- src/Component/Dashboard/index.js
+++ src/Component/Dashboard/index.js
@@ -1,7 +1,87 @@
 import React from 'react';
+import FormatQuoteRoundedIcon from '@mui/icons-material/FormatQuoteRounded';
+import CheckCircleTwoToneIcon from '@mui/icons-material/CheckCircleTwoTone';
+import './Dashboard.css'
 
 export default function Dashboard() {
-	return (<div>
-		<h1>Dashboard</h1>
-	</div>);
+	return (
+		<>
+			<div className='dashboard'>
+				<section className='profile'>
+					<div>
+						<h1>Hi Neeraj,</h1>
+						<div className='quote'>
+							<p><FormatQuoteRoundedIcon sx={{fontSize:'40px'}}/>Nemo enim ipsam voluptatem quia voluptas sit aspernatur</p>
+							<span>A+</span>
+						</div>
+					</div>
+					<div className='services'>
+						<div className='rec_post'>
+							<p>Recent Request</p>
+							<div className='req_contain'>
+								<div className='request'>
+									<span className='bl_type_req'>AB+</span>
+									<p style={{margin:'0 0 0 15px'}}>Neeraj Panmand<br/><span>Pune</span></p>
+									<CheckCircleTwoToneIcon sx={{margin:'0 10px 0 auto'}} color='success'/>
+								</div>
+								<div className='request'>
+									<span className='bl_type_req'>AB+</span>
+									<p style={{margin:'0 0 0 15px'}}>Neeraj Panmand<br/><span>Pune</span></p>
+									<CheckCircleTwoToneIcon sx={{margin:'0 10px 0 auto'}} color='success'/>
+								</div>
+								<div className='request'>
+									<span className='bl_type_req'>AB+</span>
+									<p style={{margin:'0 0 0 15px'}}>Neeraj Panmand<br/><span>Pune</span></p>
+									<CheckCircleTwoToneIcon sx={{margin:'0 10px 0 auto'}} color='success'/>
+								</div>
+								<div className='request'>
+									<span className='bl_type_req'>AB+</span>
+									<p style={{margin:'0 0 0 15px'}}>Neeraj Panmand<br/><span>Pune</span></p>
+									<CheckCircleTwoToneIcon sx={{margin:'0 10px 0 auto'}} color='success'/>
+								</div>
+								<div className='request'>
+									<span className='bl_type_req'>AB+</span>
+									<p style={{margin:'0 0 0 15px'}}>Neeraj Panmand<br/><span>Pune</span></p>
+									<CheckCircleTwoToneIcon sx={{margin:'0 10px 0 auto'}} color='success'/>
+								</div>
+								<div className='request'>
+									<span className='bl_type_req'>AB+</span>
+									<p style={{margin:'0 0 0 15px'}}>Neeraj Panmand<br/><span>Pune</span></p>
+									<CheckCircleTwoToneIcon sx={{margin:'0 10px 0 auto'}} color='success'/>
+								</div>
+							</div>
+						</div>
+						<div className='achive'>
+							<p>Extra div</p>
+						</div>
+					</div>
+				</section>
+				<section className='notification'>
+					<h3>Notification</h3>
+					<div className='notice_list'>
+						<div className='list-bg'>
+							<div className='list'>
+
+							</div>
+						</div>
+						<div className='list-bg'>
+							<div className='list'>
+
+							</div>
+						</div>
+						<div className='list-bg'>
+							<div className='list'>
+
+							</div>
+						</div>
+						<div className='list-bg'>
+							<div className='list'>
+
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+		</>
+	);
 }
